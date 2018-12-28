dbc32a6652bff43c5216327c44033ff778abe850
murdockhou <murdockhou@gmail.com>
2018-12-28 12:04:45 +0800

    fix get_heatmap_label bug

--- src/get_heatmap.py
+++ src/get_heatmap.py
@@ -34,27 +34,39 @@ def get_heatmap(keypoints, ori_height, ori_width, heatmap_height, heatmap_width,
 
 def gaussian(heatmap, center_x, center_y, sigma):
 
-    th = 1.6052
+    th = 4.6052
     delta = math.sqrt(th * 2)
 
     height = heatmap.shape[0]
-    width  = heatmap.shape[1]
+    width = heatmap.shape[1]
 
-    x0 = int(max(0, center_x - delta * sigma))
-    y0 = int(max(0, center_y - delta * sigma))
+    x0 = int(max(0, center_x - delta * sigma + 0.5))
+    y0 = int(max(0, center_y - delta * sigma + 0.5))
 
-    x1 = int(min(width, center_x + delta * sigma))
-    y1 = int(min(height, center_y + delta * sigma))
+    x1 = int(min(width - 1, center_x + delta * sigma + 0.5))
+    y1 = int(min(height - 1, center_y + delta * sigma + 0.5))
 
-    for y in range(y0, y1):
-        for x in range(x0, x1):
-            d   = (x - center_x) ** 2 + (y - center_y) ** 2
-            exp = d / 2.0 / sigma / sigma
+    ## fast way
+    arr_heat = heatmap[y0:y1 + 1, x0:x1 + 1]
+    exp_factor = 1 / 2.0 / sigma / sigma
+    x_vec = (np.arange(x0, x1 + 1) - center_x) ** 2
+    y_vec = (np.arange(y0, y1 + 1) - center_y) ** 2
+    xv, yv = np.meshgrid(x_vec, y_vec)
+    arr_sum = exp_factor * (xv + yv)
+    arr_exp = np.exp(-arr_sum)
+    arr_exp[arr_sum > th] = 0
+    heatmap[y0:y1 + 1, x0:x1 + 1] = np.maximum(arr_heat, arr_exp)
 
-            if exp > th:
-                continue
-            heatmap[y][x] = max(heatmap[y][x], math.exp(-exp))
-            heatmap[y][x] = min(heatmap[y][x], 1.0)
+    ## slow way
+    # for y in range(y0, y1):
+    #     for x in range(x0, x1):
+    #         d = (x - center_x) ** 2 + (y - center_y) ** 2
+    #         exp = d / 2.0 / sigma / sigma
+    #         if exp > th:
+    #             continue
+    #
+    #         heatmap[y][x] = max(heatmap[y][x], math.exp(-exp))
+    #         heatmap[y][x] = min(heatmap[y][x], 1.0)
 
     return heatmap
 
