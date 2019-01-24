e319d768af6e0891c34a619c52183fa84d079874
murdockhou <murdockhou@gmail.com>
2019-01-24 12:35:53 +0800

    edit train_config.py

--- src/train_config.py
+++ src/train_config.py
@@ -10,13 +10,13 @@
 train_config = {}
 
 # dataset parameters
-# train_config['train_json_file'] = '/media/ulsee/E/ai_format_dataset/total_ai_format_annos.json'
-# train_config['train_data_path'] = '/media/ulsee/E/ai_format_dataset/imgs'
+train_config['train_json_file'] = '/media/ulsee/E/ai_format_dataset/total_ai_format_annos.json'
+train_config['train_data_path'] = '/media/ulsee/E/ai_format_dataset/imgs'
 train_config['valid_batch_size']=1
 train_config['train_nums']      = 372401
 train_config['valid_nums']      = 30000
-train_config['train_json_file'] = '/media/ulsee/E/datasets/ai_challenger_valid_test/ai_challenger_keypoint_validation_20170911/keypoint_validation_annotations_20170911.json'
-train_config['train_data_path'] = '/media/ulsee/E/datasets/ai_challenger_valid_test/ai_challenger_keypoint_validation_20170911/keypoint_validation_images_20170911'
+train_config['valid_json_file'] = '/media/ulsee/E/datasets/ai_challenger_valid_test/ai_challenger_keypoint_validation_20170911/keypoint_validation_annotations_20170911.json'
+train_config['valid_data_path'] = '/media/ulsee/E/datasets/ai_challenger_valid_test/ai_challenger_keypoint_validation_20170911/keypoint_validation_images_20170911'
 
 # train params
 train_config['batch_size'] = 36
@@ -30,17 +30,7 @@ train_config['paf_width_thre']  = 1.5
 train_config['save_models']     = 100
 train_config['input_scale']     = 4
 
-# tri_hourglass
-# train_config['log_path'] = 'logs/tri_hourglass.log'
-# train_config['finetuning'] = None
-# train_config['checkpoint_path'] = '/home/hsw/tri_hourglass/'
-
-# hourglass
-# train_config['log_path'] = 'logs/hourglass.log'
-# train_config['finetuning'] = None
-# train_config['checkpoint_path'] = '/home/hsw/hourglass/'
-
-# head_neck_count_ori
+# model path
 train_config['log_path'] = 'logs/head_neck_count_2.log'
 train_config['finetuning'] = None
 train_config['checkpoint_path'] = '/home/hsw/head_neck_count_2/'
